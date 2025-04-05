cc59edacfdd7fa57e867456ddf956f3b0922883a
NRCHST <142297515+NRCHST@users.noreply.github.com>
2025-04-05 11:41:17 +0200

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -8,18 +8,15 @@ def home():
     return "Bitmap Wallet Analyzer backend is running."
 
 @app.route("/wallet/<address>")
-def get_wallet_data(address):
+def get_inscriptions(address):
     try:
-        url = f"https://open-api.unisat.io/v1/indexer/address/{address}/inscription-summary"
-
+        url = f"https://open-api.unisat.io/v1/indexer/address/{address}/inscriptions?size=100"
         headers = {
             "accept": "application/json",
             "Authorization": "Bearer f17b2e8795cc08181ac1d553868f31d7d9a5a78ba94a57568f0b2cc5b2c6bf72"
         }
-
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         return jsonify(response.json())
-
     except Exception as e:
         return jsonify({"error": str(e)}), 500
