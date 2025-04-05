5fca2995f451aa8b9a117ade029c4294bba61220
NRCHST <142297515+NRCHST@users.noreply.github.com>
2025-04-05 11:51:48 +0200

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -10,13 +10,16 @@ def home():
 @app.route("/wallet/<address>")
 def get_inscriptions(address):
     try:
-        url = f"https://open-api.unisat.io/v1/indexer/address/{address}/inscriptions?size=100"
+        url = f"https://open-api.unisat.io/v1/indexer/address/{address}/inscription-data?cursor=0&size=16"
+
         headers = {
             "accept": "application/json",
             "Authorization": "Bearer f17b2e8795cc08181ac1d553868f31d7d9a5a78ba94a57568f0b2cc5b2c6bf72"
         }
+
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         return jsonify(response.json())
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
