3300503114d7259a5914b2d212eef3627fd8f9c2
NRCHST <142297515+NRCHST@users.noreply.github.com>
2025-04-05 12:40:00 +0200

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
-CORS(app)  # <== DIT IS CRUCIAAL
+CORS(app)
 
 @app.route("/")
 def home():
@@ -12,8 +12,7 @@ def home():
 @app.route("/wallet/<address>")
 def get_inscriptions(address):
     try:
-        url = f"https://open-api.unisat.io/v1/indexer/address/{address}/inscription-data?cursor=0&size=16"
-
+        url = f"https://open-api.unisat.io/v1/indexer/address/{address}/inscription-data?cursor=0&size=100"
         headers = {
             "accept": "application/json",
             "Authorization": "Bearer f17b2e8795cc08181ac1d553868f31d7d9a5a78ba94a57568f0b2cc5b2c6bf72"
@@ -21,7 +20,11 @@ def get_inscriptions(address):
 
         response = requests.get(url, headers=headers)
         response.raise_for_status()
-        return jsonify(response.json())
+
+        resp = make_response(response.text)
+        resp.headers["Access-Control-Allow-Origin"] = "*"
+        resp.headers["Content-Type"] = "application/json"
+        return resp
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
